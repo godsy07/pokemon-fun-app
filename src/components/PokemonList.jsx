60c0fcf3b823bb8fb12014a54934a36c3a56d299
unknown <godsonemmanuel07@gmail.com>
2021-07-12 13:20:19 +0530

    Added pokemon images and abilities

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,19 +1,42 @@
 import React, { useState, useEffect } from "react";
 
 const PokemonList = ({ poke }) => {
-  // const [qualities, setQualities] = useState([]);
+  // Setting up variables for each Element
+  const [abilities, setAbilities] = useState([]);
+  const [sprites, setSprites] = useState([]);
+  const [image, setImage] = useState();
+  // const [moves, setMoves] = useState([]);
 
-  // useEffect(() => {
-  //   fetch(poke.url).then(res => res.json())
-  //   .then(data => data)
-  // }, [])
-
-  // src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+  useEffect(() => {
+    fetch(poke.url)
+      .then((res) => res.json())
+      .then((data) => {
+        setAbilities(data.abilities);
+        setSprites(data.sprites);
+        setImage(data.sprites.other.dream_world.front_default);
+        // setMoves(data.moves);
+      });
+  }, []);
 
   return (
     <div className='card-info'>
-      {/* <img src='' /> */}
-      <p>{poke.name.toUpperCase()}</p>
+      <div className='card-images'>
+        {/* {<img src={sprites.other.dream_world.front_default} />} */}
+        {/* <img src={sprites.front_default} /> */}
+        <img src={image} />
+      </div>
+
+      <p className='name'>{poke.name.toUpperCase()}</p>
+      <div className='abilities'>
+        <h4>Abilities: </h4>
+        <div>
+          {abilities.map((e, idx) => (
+            <p key={idx}>
+              {idx + 1}. {e.ability.name}
+            </p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
